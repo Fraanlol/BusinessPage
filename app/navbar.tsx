5cbcf2b2c5c7d1feb53bac104f021394c80083a4
Franco Catas <franco.catas@hotmail.com>
2023-10-25 19:33:54 -0300

    Added burguer animation

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,6 +1,15 @@
+'use client'
 import Image from 'next/image'
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import AionData from '../public/icons8-menu.json'
+import { useRef } from 'react';
 
 export default function NavBar() {
+    const playerRef = useRef<Player>(null);
+
+    playerRef.current?.setSeeker(60);
+
+
     return (
       <nav className="flex flex-row justify-around items-center p-5 bg-white h-32">
         <div className="flex-1 text-3xl text-center flex flex-row justify-start lg:justify-center items-center">
@@ -20,9 +29,9 @@ export default function NavBar() {
             <li className='px-6 hover:border-b cursor-pointer'><a href="#">FAQ</a></li>
           </ul>
         </div>
-        <div className="flex-1 text-xl font-thin lg:hidden flex justify-end pr-5">
-          <Image src={'/icon-menu.svg'} height={30} width={30} alt='' className='lg:hidden cursor-pointer'/>
-          <ul className="flex flex-col absolute top-0 right-0 bg-navbur bg-opacity-100 p-20 w-full justify-start items-center hidden">
+        <div className="flex-1 text-xl font-thin lg:hidden flex justify-end pt-6">
+          <Player src={AionData} ref={playerRef}  style={{ height: '50px', width: '50px' }}></Player>
+          <ul id='bgmenu' className="flex flex-col absolute top-0 right-0 bg-navbur bg-opacity-100 p-20 w-full justify-start items-center hidden">
             <li className='py-6 hover:border-b cursor-pointer'>Servicios</li>
             <li className='py-6 hover:border-b cursor-pointer'><a href="#">Blog</a></li>
             <li className='py-6 hover:border-b cursor-pointer'><a href="#">Clientes</a></li>
